3899ae6ec167e0327dc7a843c93855fb9a996b80
Nandha <nandha.kumar790@gmail.com>
2024-08-24 19:03:00 +0530

    update move zeroes

--- Top Interview Questions/Array/Move Zeroes/Solution.php
+++ Top Interview Questions/Array/Move Zeroes/Solution.php
@@ -13,23 +13,31 @@ class Solution {
             $moved = 0;
             for ($i = 0; $i < count($nums); $i++ ){
                 echo $i;
-                if (count($nums) - $i - 1 == $moved){
+                if (count($nums) == $i + 1 + $moved){
                     break;
                 }
                 if ($nums[$i] == 0){
                     array_splice($nums, $i, 1);
                     $nums[] = 0;
                     $moved++;
+                    $i--;
                 }
+            }
+        }
+    }
+    // This is the better solution
+    function moveZeroes2(&$nums) {
+        for($i = 0; $i < count($nums); $i++){
+            if($nums[$i] === 0){
+                unset($nums[$i]);
+                $nums[] = 0;
             }
         }
     }
 
 }	
-	$nums = array(1,0,0,3,12);
+	$nums = array(0,0,1,0,0,3,12,0,0,0);
 	$obj = new Solution();
-	$obj->moveZeroes($nums);
-	print_r($nums);
-    print_r(array(1,3,12,0,0));
-
+	$obj->moveZeroes2($nums);
+    print_r($nums);
 ?>
